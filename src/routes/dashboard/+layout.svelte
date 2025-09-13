f61b35507d3a7cce178f1f8123a85e903f5ba82b
devnull03 <56480041+devnull03@users.noreply.github.com>
2025-09-13 16:45:18 -0400

    feat: added sidebar boilerplate

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import SiteHeader from "$lib/components/site-header.svelte";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import type { LayoutProps } from "./$types";
+	
+	let props: LayoutProps = $props();
+
+</script>
+
+<div class="[--header-height:calc(--spacing(14))]">
+	<Sidebar.Provider class="flex flex-col">
+		<SiteHeader />
+		<div class="flex flex-1">
+			<AppSidebar />
+			<Sidebar.Inset>
+
+				{@render props.children?.()}
+
+			</Sidebar.Inset>
+		</div>
+	</Sidebar.Provider>
+</div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
+		exclude: ['@ai-sdk/svelte', '@ai-sdk/openai'],
+		include: ['clsx', 'tailwind-merge']
+	},
+	server: {
+		fs: {
+			strict: false
+		}
+	}
 });

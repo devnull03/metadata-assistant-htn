67f7fa1be7d8b97fff0ae58ebc5097ced88e3477
MelnCat <melncatuwu@gmail.com>
2025-09-13 13:53:13 -0700

    wtf

---
+++ src/routes/new/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { showDirectoryPicker } from "native-file-system-adapter";
+
+	const selectFolder = async () => {
+		const dir = await showDirectoryPicker();
+		console.log(dir);
+	};
+</script>
+
+<button onclick={selectFolder}>select folder</button>

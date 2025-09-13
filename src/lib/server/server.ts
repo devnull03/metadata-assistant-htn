613c1345dabfdaea7f9ee2f5cca42756eb586055
aradsab <ediarad@yahoo.co.uk>
2025-09-13 16:31:05 -0400

    Update server.ts

--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -1,19 +1,28 @@
 import { COHERE_API_KEY } from "$env/static/private";
 
 
-const { CohereClientV2 } = require('cohere-ai');
+import { CohereClientV2 } from 'cohere-ai';
 const cohere = new CohereClientV2({
   token: COHERE_API_KEY,
 });
 (async () => {
   const response = await cohere.chat({
-    model: 'command-a-03-2025',
+    model: 'command-a-vision-07-2025',
     messages: [
       {
         role: 'user',
-        content: 'hello world!',
+        content: [
+          { type: 'text', text: 'Describe the logo!' },
+          {
+            type: 'image_url',
+            imageUrl: {
+              url: 'https://cohere.com/favicon-32x32.png',
+              detail: 'auto',
+            },
+          },
+        ],
       },
     ],
   });
-  console.log(response);
+  console.log(response.message.content[0].text);
 })();

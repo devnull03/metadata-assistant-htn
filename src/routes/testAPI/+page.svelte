03443a8232af5a50bb6d8dcdccf4cf3990c999a0
aradsab <ediarad@yahoo.co.uk>
2025-09-13 18:59:52 -0400

    fix remote

--- src/routes/testAPI/+page.svelte
+++ src/routes/testAPI/+page.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-  let result = '';
+    import { onMount } from "svelte";
+    import { getPosts } from './data.remote'
 
-  async function callEndpoint() {
-    const res = await fetch('/api/assistant', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ prompt: 'Describe the logo!' })
-    });
-    const data = await res.json();
-    result = JSON.stringify(data, null, 2);
-  }
+    onMount(async () => {
+        await getPosts();
+    })
+
 </script>
-
-<button on:click={callEndpoint}>Call Assistant</button>
-<pre>{result}</pre>

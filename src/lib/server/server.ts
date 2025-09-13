97b35909de602849c50be28e0f52c88d27df33f8
aradsab <ediarad@yahoo.co.uk>
2025-09-13 15:11:37 -0400

    added server file

--- src/lib/server/server.ts
+++ src/lib/server/server.ts
@@ -1,3 +1,19 @@
-import { SOME_API } from "$env/static/private";
+import { COHERE_API_KEY } from "$env/static/private";
 
 
+const { CohereClientV2 } = require('cohere-ai');
+const cohere = new CohereClientV2({
+  token: COHERE_API_KEY,
+});
+(async () => {
+  const response = await cohere.chat({
+    model: 'command-a-03-2025',
+    messages: [
+      {
+        role: 'user',
+        content: 'hello world!',
+      },
+    ],
+  });
+  console.log(response);
+})();

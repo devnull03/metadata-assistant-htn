f1d61a8ff7c34566fa5044677f78428c2b11db49
aradsab <ediarad@yahoo.co.uk>
2025-09-13 16:56:34 -0400

    Create +page.svelte

---
+++ src/routes/testAPI/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+    import { CohereClientV2 } from 'cohere-ai';
+    const cohere = new CohereClientV2({
+        token: "COHERE_API_KEY",
+    });
+    export async function queryCohere() {
+        const response = await cohere.chat({
+            model: 'command-a-vision-07-2025',
+            messages: [
+            {
+                role: 'user',
+                content: [
+                { type: 'text', text: 'Describe the logo!' },
+                {
+                    type: 'image_url',
+                    imageUrl: {
+                    url: 'https://cohere.com/favicon-32x32.png',
+                    detail: 'auto',
+                    },
+                },
+                ],
+            },
+            ],
+        });
+
+        console.log(response.message.content);
+        return response.message.content;
+}
+    import { onMount } from "svelte";
+    onMount(async () => {
+        await queryCohere();
+    });
+</script>
+
+<h1>Recent posts</h1>

2569c60ea2f5d8adb38dc2afecd8f762038dc1a6
MelnCat <melncatuwu@gmail.com>
2025-09-13 23:25:08 -0700

    styles

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -37,6 +37,24 @@
 		background-color: var(--dark);
 		font-family: system-ui, sans-serif;
 		color: var(--text);
+		--s: 193px; /* control the size*/
+		--c1: #921c2d;
+		--c2: #880d1e;
+		--c3: #5b0f1a;
+
+		--_g: var(--c1) 10%, var(--c2) 10.5% 19%, #0000 19.5% 80.5%, var(--c2) 81% 89.5%, var(--c3) 90%;
+		--_c: from -90deg at 37.5% 50%, #0000 75%;
+		--_l1: linear-gradient(145deg, var(--_g));
+		--_l2: linear-gradient(35deg, var(--_g));
+		background:
+			var(--_l1),
+			var(--_l1) calc(var(--s) / 2) var(--s),
+			var(--_l2),
+			var(--_l2) calc(var(--s) / 2) var(--s),
+			conic-gradient(var(--_c), var(--c1) 0) calc(var(--s) / 8) 0,
+			conic-gradient(var(--_c), var(--c3) 0) calc(var(--s) / 2) 0,
+			linear-gradient(90deg, var(--c3) 38%, var(--c1) 0 50%, var(--c3) 0 62%, var(--c1) 0);
+		background-size: var(--s) calc(2 * var(--s) / 3);
 	}
 
 	/* Card */
--- src/routes/new/projectbuttons.svelte
+++ src/routes/new/projectbuttons.svelte
@@ -1,75 +1,73 @@
 <script lang="ts">
-    import { queryCohereRemote } from './data.remote';
-    import { getUploadedFiles } from "$lib/utils/files";
-    import * as kv from "idb-keyval";
-    let folder = $state<[string, FileSystemFileHandle][]>([]);
-    folder = (await getUploadedFiles()) ?? [];
-    const selectFolder = async () => {
-        const dir = await showDirectoryPicker();
-        await dir.requestPermission();
-        const entries = [];
-        for await (const entry of dir.entries()) {
-            if (entry[1].kind !== "file") continue;
-            entries.push(entry);
-        }
-        // TODO
-        await batchQuery(entries)
-        await kv.set("images", dir);
-        location.assign("./dashboard");
-    };
+	import { queryCohereRemote } from "./data.remote";
+	import { getUploadedFiles } from "$lib/utils/files";
+	import * as kv from "idb-keyval";
+	import { goto } from "$app/navigation";
+	let folder = $state<[string, FileSystemFileHandle][]>([]);
+	folder = (await getUploadedFiles()) ?? [];
+	const selectFolder = async () => {
+		const dir = await showDirectoryPicker();
+		await dir.requestPermission();
+		const entries = [];
+		for await (const entry of dir.entries()) {
+			if (entry[1].kind !== "file") continue;
+			entries.push(entry);
+		}
+		// TODO
+		await batchQuery(entries);
+		await kv.set("images", dir);
+		goto("./dashboard");
+	};
 
-
-
-
-    async function batchQuery(entries: [string, FileSystemFileHandle | FileSystemDirectoryHandle][]) {
-        for (const entry of entries) {
-            const handle = entry[1];
-            if (handle.kind === "file") {
-                const file = await handle.getFile();
-                const arrayBuffer = await file.arrayBuffer();
-                const imageBase64 = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
-                await queryCohereRemote({ image: imageBase64 });
-            }
-        }
-    }
+	async function batchQuery(entries: [string, FileSystemFileHandle | FileSystemDirectoryHandle][]) {
+		for (const entry of entries) {
+			const handle = entry[1];
+			if (handle.kind === "file") {
+				const file = await handle.getFile();
+				const arrayBuffer = await file.arrayBuffer();
+				const imageBase64 = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+				await queryCohereRemote({ image: imageBase64 });
+			}
+		}
+	}
 </script>
 
 <article>
-    {#if folder.length}
-        <button
-            onclick={() => {
-                location.assign("./dashboard");
-            }}
-        >
-            <h2>Existing Project</h2>
-            <p>{folder.length} images</p></button
-        >{/if}
-    <button onclick={selectFolder}
-        ><h2>New Project</h2>
-        <p>
-            {folder.length ? "WARNING: This will delete your current project." : "Creates a new project."}
-        </p></button
-    >
+	{#if folder.length}
+		<button
+			onclick={() => {
+				goto("./dashboard");
+			}}
+		>
+			<h2>Existing Project</h2>
+			<p>{folder.length} images</p></button
+		>{/if}
+	<button onclick={selectFolder}
+		><h2>New Project</h2>
+		<p>
+			{folder.length ? "WARNING: This will delete your current project." : "Creates a new project."}
+		</p></button
+	>
 </article>
 
 <style>
-    article {
-        display: flex;
-        gap: 1em;
-    }
-    h2 {
-        font-weight: bold;
-    }
+	article {
+		display: flex;
+		gap: 1em;
+	}
+	h2 {
+		font-weight: bold;
+	}
 
-    button {
-        background-color: var(--dark);
-        color: var(--light);
-        border: var(--dark) 2px solid;
-        padding: 0.5em 1em;
-        cursor: pointer;
-        &:hover {
-            background-color: var(--light);
-            color: var(--dark);
-        }
-    }
+	button {
+		background-color: var(--dark);
+		color: var(--light);
+		border: var(--dark) 2px solid;
+		padding: 0.5em 1em;
+		cursor: pointer;
+		&:hover {
+			background-color: var(--light);
+			color: var(--dark);
+		}
+	}
 </style>

c89c790ac75fab8065a246b44719d1957b6a6477
aradsab <ediarad@yahoo.co.uk>
2025-09-13 21:14:04 -0400

    added modal

--- src/lib/components/item-view-modal/item-view-modal.svelte
+++ src/lib/components/item-view-modal/item-view-modal.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-	
+
+     import * as Dialog from "$lib/components/ui/dialog/index.js";
+
+    
 interface ItemViewModalProps {
     isOpen: boolean;
-    onClose: () => void;
+    onClose?: () => void;
 
     img: string;
     item: Record<string, any> | null;
 
 }
 
+let { isOpen = $bindable(), onClose, img, item}: ItemViewModalProps = $props()
+
 </script>
-
-<div>
-
-
-	
-</div>
+ 
+<Dialog.Root bind:open={isOpen}>
+ <Dialog.Trigger></Dialog.Trigger>
+ <Dialog.Content>
+  <Dialog.Header>
+   <Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+   <Dialog.Description>
+    This action cannot be undone. This will permanently delete your account
+    and remove your data from our servers.
+   </Dialog.Description>
+  </Dialog.Header>
+ </Dialog.Content>
+</Dialog.Root>
